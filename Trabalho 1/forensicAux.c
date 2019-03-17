f0a1a1ea6af13ac9063e31a10c50814681b08b2a
Eduardo Ribeiro <EduRibeiro@AirdeEduardo2.lan>
2019-03-17 15:04:36 +0000

    Utilizacao de pipes para extrair os resultados dos comandos

--- Trabalho 1/forensicAux.c
+++ Trabalho 1/forensicAux.c
@@ -9,66 +9,28 @@
 #include <fcntl.h>
 #include <string.h>
 
+#define MAX_SIZE    255
+
 void commandToString(char* result, int max_size, char* command, char* file){
 
-        int fd;
+        FILE* fileOut;
 
-        pid_t pid = fork(); //creates new child process
-        if(pid == -1){
-            perror("fork");
-            exit(3);
-        }
-        else if(pid > 0){ //parent process
+        char pipeCommand[MAX_SIZE];
+        strcpy(pipeCommand, command);
+        strcat(pipeCommand, " ");
+        strcat(pipeCommand, file);
 
-            int status;
-            pid_t childPid = wait(&status); //waits for child
-            if(childPid == -1 || WEXITSTATUS(status) != 0){
-                printf("Problem with wait/child!\n");
-                exit(3);
-            }
-        }
-        else if(pid == 0){ //child process
-
-            //creates a new file, in order to store the output of the command call
-            if((fd = open("123.txt", O_RDWR | O_CREAT, 0644)) == -1){
-                perror("Temporary file");
-                exit(3);
-            }
-
-            if(dup2(fd, STDOUT_FILENO) == -1){ //the standard output now goes to the file previously created
-                perror("dup2");
-                exit(3);
-            }
-
-            execlp(command, command, file, NULL); //executes command
-            printf("Error with exec!");
+        if((fileOut = popen(pipeCommand, "r")) == NULL){ //uses a pipe in order to get the output of the command
+            printf("Popen error!\n");
             exit(3);
         }
 
-
-        if((fd = open("123.txt", O_RDWR, 0644)) == -1){ //opens the file (this time in the parent process)
-            perror("Temporary file");
+        if(fgets(result, max_size, fileOut) == NULL){
+            printf("Error with fgets!\n");
             exit(3);
         }
 
-        int rb;
-        if((rb = read(fd, result, max_size)) <= 0){
-            perror("Error with read");
-            exit(3);
-        }
-
-        result[rb - 1] = 0; //gets rid of the newline
-
-        if(close(fd) == -1){ //closes the file descriptor
-            perror("close");
-            exit(3);
-        }
-
-        if(unlink("123.txt") == -1){ //deletes the temporary file
-            perror("unlink");
-            exit(3);
-        }
-
+        result[strlen(result) - 1] = 0; //gets rid of the newline
 }
 
 //-------------------------
